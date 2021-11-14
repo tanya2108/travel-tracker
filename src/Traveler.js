656a0bc0dedc163d624c2068afc97b76f9c36764
Tanya Zhuge <tangnazhuge@gmail.com>
2021-11-13 20:42:44 -0800

    CCreate tests for traveler class

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,7 +1,11 @@
 class Traveler {
-  constructors() {
-
+  constructor(travelerData) {
+    this.id = travelerData.id;
+    this.name = travelerData.name;
+    this.travelerType = travelerData.travelerType;
   }
 
 }
+
+
 module.exports = Traveler;
