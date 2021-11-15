fc3a4195e3fde9b2f26c2bde072934d01b36b029
Tanya Zhuge <tangnazhuge@gmail.com>
2021-11-14 16:05:50 -0800

    Add tests for Trips and Destinations

--- test/Destinations-Test.js
+++ test/Destinations-Test.js
@@ -1,2 +1,62 @@
 const expect = require ('chai').expect;
 const Destinations = require ('../src/Destinations.js');
+
+describe('Destinations', () => {
+
+  let destinations;
+
+  beforeEach(function() {
+    const destinationsData = [{
+      id: 1,
+      destination: 'Lima, Peru',
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+      alt: 'overview of city buildings with a clear sky'
+    },
+    {
+      id: 2,
+      destination: 'Stockholm, Sweden',
+      estimatedLodgingCostPerDay: 100,
+      estimatedFlightCostPerPerson: 780,
+      image: 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'city with boats on the water during the day time'
+    },
+    {
+      id: 3,
+      destination: 'Sydney, Austrailia',
+      estimatedLodgingCostPerDay: 130,
+      estimatedFlightCostPerPerson: 950,
+      image: 'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'opera house and city buildings on the water with boats'
+    }];
+    destinations = new Destinations(destinationsData)
+  });
+
+  it('should be a function', function() {
+    expect(Destinations).to.be.a('function');
+  });
+
+  it('should be an instance of Destinations', function() {
+    expect(destinations).to.be.an.instanceOf(Destinations);
+  });
+
+  it('should retrieve a particular destination based on ID', function() {
+    expect(destinations.retrieveDestinationData(2)).to.deep.equal(
+      {
+      id: 2,
+      destination: 'Stockholm, Sweden',
+      estimatedLodgingCostPerDay: 100,
+      estimatedFlightCostPerPerson: 780,
+      image: 'https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'city with boats on the water during the day time'
+      }
+    );
+  });
+
+  it('should return an error message if destination does not exist for user', function() {
+    expect(destinations.retrieveDestinationData(6)).to.deep.equal('Sorry, this destination is not available yet.');
+
+  });
+
+  });
--- test/Trips-Test.js
+++ test/Trips-Test.js
@@ -1,2 +1,67 @@
 const expect = require ('chai').expect;
 const Trips = require ('../src/Trips.js');
+
+describe('Trips', () => {
+
+  let trips;
+
+  beforeEach(function() {
+    const tripsData = [{
+    id: 1,
+    userID: 2,
+    destinationID: 1,
+    travelers: 1,
+    date: '2022/09/16',
+    duration: 8,
+    status: 'approved',
+    suggestedActivities: []
+  },
+  { id: 2,
+    userID: 1,
+    destinationID: 3,
+    travelers: 2,
+    date: '2022/08/02',
+    duration: 5,
+    status: ' approved',
+    suggestedActivities:[]
+  },
+  { id: 3,
+    userID: 3,
+    destinationID: 1,
+    travelers: 5,
+    date: '2022/05/20',
+    duration: 3,
+    status: 'approved',
+    suggestedActivities: []
+  }];
+    trips = new Trips(tripsData)
+  });
+
+  it('should be a function', function() {
+    expect(Trips).to.be.a('function');
+  });
+
+  it('should be an instance of Trip', function() {
+    expect(trips).to.be.an.instanceOf(Trips);
+  });
+
+  it('should retrieve a user trip data', function() {
+    expect(trips.retrieveTripData(2)).to.deep.equal(
+      [{ id: 1,
+        userID: 2,
+        destinationID: 1,
+        travelers: 1,
+        date: '2022/09/16',
+        duration: 8,
+        status: 'approved',
+        suggestedActivities: []
+      }]
+    );
+  });
+
+  it('should return an error message if trip data does not exist for user', function() {
+    expect(trips.retrieveTripData(5)).to.deep.equal('Sorry, traveler5 has not made any trip yet.');
+
+  });
+
+  });
