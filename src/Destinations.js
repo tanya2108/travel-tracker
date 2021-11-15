f838be4c016d3902f52dfded23315771eeadcad2
Tanya Zhuge <tangnazhuge@gmail.com>
2021-11-14 16:06:20 -0800

    Complete destinations and trips class

--- src/Destinations.js
+++ src/Destinations.js
@@ -1,9 +1,18 @@
 class Destinations {
-
-  constructors() {
-
+  constructor(destinations) {
+    this.data = destinations;
   }
 
+retrieveDestinationData(destinationID) {
+  const specificDestination = this.data.find((destination) => {
+    return destination.id === destinationID  ;
+  })
+  if (!specificDestination) {
+    return `Sorry, this destination is not available yet.`
+  } else {
+    return specificDestination;
+  }
+ }
 }
 
 module.exports = Destinations;
--- src/Trips.js
+++ src/Trips.js
@@ -1,7 +1,19 @@
 class Trips {
-  constructors() {
+  constructor(trips) {
+    this.data = trips;
+  }
 
+  retrieveTripData(userID) {
+    const userTripsData = this.data.filter((eachTrip) => {
+      return eachTrip.userID === userID;
+    });
+    if (userTripsData.length === 0){
+      return `Sorry, traveler${userID} has not made any trip yet.`
+    } else {
+      return userTripsData;
+    }
   }
 
 }
+
 module.exports = Trips;
