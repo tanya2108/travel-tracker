aac7b9c7f87f1c6d5842b8d22e80102ccc684555
Tanya Zhuge <tangnazhuge@gmail.com>
2021-11-16 11:20:49 -0800

    Add Post

--- src/api.js
+++ src/api.js
@@ -1,7 +1,17 @@
 const fetchData = (param) => {
   return fetch(`http://localhost:3001/api/v1/${param}`)
     .then(response => response.json())
-    .catch(error => console.log(error))
+}
+
+const addTrip = (object) => {
+  return fetch('http://localhost:3001/api/v1/trips', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(object)
+  })
+    .then(response => response.json())
 }
 
 
@@ -12,4 +22,5 @@ const fetchData = (param) => {
 
 export {
   fetchData,
+  addTrip,
 };
